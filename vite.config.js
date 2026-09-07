1b263290be5b8eece462052e316d22a5910a13e7
Akash K <akashkarnan02@gmail.com>
2026-09-07 22:34:43 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), viteReact()],
+  plugins: [tanstackStart(), nitro(), viteReact()],
 })
 
 export default config
+
